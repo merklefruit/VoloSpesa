bf788af38ce5d21b61a19b0554ab86ed32fd081a
Nicolas <nicolas@racchi.com>
2020-04-25 12:21:11 +0200

    💫 Update

--- client/src/pages/Volontari/Volontari.js
+++ client/src/pages/Volontari/Volontari.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { Typography, Card, Form, Input } from "antd";
+import { Typography, Card, Form, Input, Button } from "antd";
 import Geosuggest from "react-geosuggest";
-
+import { addUser } from "../../api/API";
 import PageLayout from "../../components/Layout/PageLayout";
 
 import "./volontari.css";
@@ -12,8 +12,11 @@ const { Title, Text } = Typography;
 export default class Volontari extends Component {
   state = {
     nome: "",
+    email: "",
     indirizzo: "",
-    telefono: ""
+    telefono: "",
+    error: false,
+    done: false
   };
 
   valueChanged = changedValues => {
@@ -23,6 +26,42 @@ export default class Volontari extends Component {
       ...prevState,
       [key]: value
     }));
+  };
+
+  onSuggestSelect = suggest => {
+    if (suggest != null) {
+      this.setState(prevState => ({
+        ...prevState,
+        ...prevState,
+        indirizzo: suggest.description,
+        latitudine: suggest.location.lat,
+        longitudine: suggest.location.lng
+      }));
+    }
+  };
+
+  formSubmitted = async event => {
+    event.preventDefault();
+    const user = {
+      nome: this.state.nome,
+      email: this.state.email,
+      indirizzo: this.state.indirizzo,
+      latitudine: this.state.latitudine,
+      longitudine: this.state.longitudine,
+      telefono: this.state.longitudine
+    };
+    const status = await addUser(user);
+    if (status !== 200) {
+      this.setState(prevState => ({
+        ...prevState,
+        error: true
+      }));
+    } else {
+      this.setState(prevState => ({
+        ...prevState,
+        done: true
+      }));
+    }
   };
 
   render() {
@@ -53,6 +92,7 @@ export default class Volontari extends Component {
           <p style={{ marginTop: 25 }}></p>
           <Card className="card">
             <div className="formdiv">
+              <Title level={2}>Iscriviti adesso</Title>
               <Form>
                 <Form
                   labelCol={{ span: 3 }}
@@ -67,6 +107,19 @@ export default class Volontari extends Component {
                     className="form-name"
                     label="Nome"
                     name="nome"
+                    rules={[
+                      {
+                        required: true,
+                        message: "Per favore, inserisci il tuo nome!"
+                      }
+                    ]}
+                  >
+                    <Input />
+                  </Form.Item>
+
+                  <Form.Item
+                    label="Email"
+                    name="email"
                     rules={[
                       {
                         required: true,
@@ -109,6 +162,13 @@ export default class Volontari extends Component {
                   >
                     <Input />
                   </Form.Item>
+                  <Button
+                    type="primary"
+                    size="large"
+                    onClick={this.formSubmitted}
+                  >
+                    Conferma
+                  </Button>
                 </Form>
               </Form>
             </div>
